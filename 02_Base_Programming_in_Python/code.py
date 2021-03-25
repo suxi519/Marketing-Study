ddae28379146079a89b5b273e6596ea7678d45c0
suxi519 <gksthgml75@naver.com>
2021-03-25 19:30:09 +0900

    Create code.py

---
+++ 02_Base_Programming_in_Python/code.py
@@ -0,0 +1,63 @@
+"""
+1.프린트(print)
+print(3*4)
+
+2.코멘트(comment)
+" 코멘트 중요하다! "
+
+3.자료형(data type)
+
+3-1.숫자(number)
+
+* 정수(integer)
+2
+
+* 소수(floating point)
+2.0
+3-2.문자열(string)
+"2" or '2'
+3-3.불린(Boolean)
+true or false
+
+4.추상화(abstraction)
+
+* 변수(variable)
+oreo = 990
+
+* 함수(function)
+def hello():
+    print("hello")
+    print("welcome to korea")
+
+hello()
+
+* 파라미터(parameter)
+def hello(name):
+    print("hello")
+    print(name)
+    print(welcome to korea)
+
+hello("sohee")
+
+* 여러 개의 파라미터
+def print_sum(a, b, c):
+    print(a + b + c)
+
+print_sum(7, 3, 1)
+
+* return
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
