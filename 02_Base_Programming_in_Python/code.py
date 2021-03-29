856c4b096f3df932e06e8dd7ae2b17701bfb9b0d
suxi519 <gksthgml75@naver.com>
2021-03-29 16:44:16 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -47,7 +47,39 @@ print_sum(7, 3, 1)
 
 * return
 """
+# 1-1. 숫자형
+# 덧셈
+print(4 + 7)
+print(4.0 + 7.0)
 
+# 뺄셈
+print(2 - 4)
+print(2.0 - 4.0)
+
+# 곱셉
+print(5 * 3)
+print(5.0 * 3.0)
+
+# 나눗셈
+print(2 / 3)
+
+# 나머지
+print(7.0 % 3.0)
+
+#거듭제곱
+print(2.0 ** 3.0)
+
+#사칙연산
+print(2 * 3 + 2)
+print(2 * (3 + 2))
+
+# 버림 나눗셈 (floor division)
+print(7 // 2)
+print(7 / 2)
+print(7.0 // 2.0)
+
+# 반올림 (round)
+print(round(3.141592, 5))
 
 
 
