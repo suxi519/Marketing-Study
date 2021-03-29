b1b409881dedf0efcec5e35c42f8d8a3c9bce13e
suxi519 <gksthgml75@naver.com>
2021-03-29 17:22:05 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -1,52 +1,4 @@
 """
-1.프린트(print)
-print(3*4)
-
-2.코멘트(comment)
-" 코멘트 중요하다! "
-
-3.자료형(data type)
-
-3-1.숫자(number)
-
-* 정수(integer)
-2
-
-* 소수(floating point)
-2.0
-3-2.문자열(string)
-"2" or '2'
-3-3.불린(Boolean)
-true or false
-
-4.추상화(abstraction)
-
-* 변수(variable)
-oreo = 990
-
-* 함수(function)
-def hello():
-    print("hello")
-    print("welcome to korea")
-
-hello()
-
-* 파라미터(parameter)
-def hello(name):
-    print("hello")
-    print(name)
-    print(welcome to korea)
-
-hello("sohee")
-
-* 여러 개의 파라미터
-def print_sum(a, b, c):
-    print(a + b + c)
-
-print_sum(7, 3, 1)
-
-* return
-"""
 # 1-1. 숫자형
 # 덧셈
 print(4 + 7)
@@ -80,6 +32,9 @@ print(7.0 // 2.0)
 
 # 반올림 (round)
 print(round(3.141592, 5))
+"""
+
+
 
 
 
