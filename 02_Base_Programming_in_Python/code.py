873b3f4b074e1df8d131592b2f1a068349fea020
suxi519 <gksthgml75@naver.com>
2021-03-31 23:23:05 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -3,6 +3,7 @@
 print(4 + 7)
 print(4.0 + 7.0)
 
+
 # 뺄셈
 print(2 - 4)
 print(2.0 - 4.0)
@@ -39,8 +40,20 @@ print("I\'m \"excited\" to learn Python!")
 print('hello' * 3)
 print('30' + '5')
 
+# 형 변환(Type Conversion)
+print(int(3.8))
+print(float(3))
+print(int("2") + int("5"))
+print(float("1.1") + float("2.5"))
+print(str(2) + str(5))
+age = 7
+print("제 나이는 " + str(age) + "살입니다.")
 
+print(10 / (10 % 6))
 
+print(4.0 + 7)
+print(2 ** 3.0)
+print(7.0 - 3)
 
 
 
