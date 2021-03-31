8a2d0731e46c185e0010095c785c253298f687c2
suxi519 <gksthgml75@naver.com>
2021-03-31 23:40:55 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -57,8 +57,9 @@ year = 2019
 month = 10
 day = 29
 print("오늘은 " + str(year) + "년 " + str(10) + "월 " + str(29) + "일 " + "입니다.")
-
-
+print("오늘은 {}년 {}월 {}일 입니다.".format(year, month, day))
+date_string = "오늘은 {}년 {}월 {}일 입니다."
+print(date_string.format(year, month, day + 1))
 
 
 
