810e59a13d7aaa62f9a7589801162e219491d2ec
suxi519 <gksthgml75@naver.com>
2021-04-01 00:44:16 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -1,8 +1,13 @@
 # 3-1. 숫자형
+# 주의 (숫자형끼리 계산 가능)
+print(10 / (10 % 6))
+print(4.0 + 7)
+print(2 ** 3.0)
+print(7.0 - 3)
+
 # 덧셈
 print(4 + 7)
 print(4.0 + 7.0)
-
 
 # 뺄셈
 print(2 - 4)
@@ -40,7 +45,7 @@ print("I\'m \"excited\" to learn Python!")
 print('hello' * 3)
 print('30' + '5')
 
-# 형 변환(Type Conversion)
+# 3-3. 형 변환(Type Conversion)
 print(int(3.8))
 print(float(3))
 print(int("2") + int("5"))
@@ -48,10 +53,8 @@ print(float("1.1") + float("2.5"))
 print(str(2) + str(5))
 age = 7
 print("제 나이는 " + str(age) + "살입니다.")
-print(10 / (10 % 6))
-print(4.0 + 7)
-print(2 ** 3.0)
-print(7.0 - 3)
+
+# 3-4. format method
 # 오늘은 2019년 10월 29일 입니다.
 year = 2019
 month = 10
@@ -60,8 +63,16 @@ print("오늘은 " + str(year) + "년 " + str(10) + "월 " + str(29) + "일 " + 
 print("오늘은 {}년 {}월 {}일 입니다.".format(year, month, day))
 date_string = "오늘은 {}년 {}월 {}일 입니다."
 print(date_string.format(year, month, day + 1))
+print("저는 {}, {}, {}를 좋아합니다!".format("사과", "배", "딸기"))
+print("저는 {2}, {1}, {0}를 좋아합니다!".format("사과", "배", "딸기"))
+num_1 = 1
+num_2 = 3
+print("{0} 나누기 {1}은 {2:.3f}입니다.".format(num_1, num_2, num_1*num_2))
+# 가장 오래된 방식 (% 기호)
+name = "한수시"
+age = "10"
 
-
+print()
 
 
 
