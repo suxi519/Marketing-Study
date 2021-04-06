674edfe28310417b4bebc7087c066017b5da43a4
suxi519 <gksthgml75@naver.com>
2021-04-07 00:52:33 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -284,16 +284,18 @@ x %= 7
 # print(square(3))
 
 # 4-9. 상수(constant)
-PI = 3.14
+# PI = 3.14
+#
+#
+# def calculate_area(r):
+#     return PI * r * r
+#
+#
+# radius = 4
+# print("반지름이 {}면, 넓이는 {}".format(radius, calculate_area(radius)))
+
+# 5-0. 스타일(style)
 
 
-def calculate_area(r):
-    return PI * r * r
 
-
-radius = 4
-print("반지름이 {}면, 넓이는 {}".format(radius, calculate_area(radius)))
-
-
-
-
+print(some_function_name())
