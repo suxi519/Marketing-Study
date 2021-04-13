7c2af767577f405dabb8554de784edafa07c9206
suxi519 <gksthgml75@naver.com>
2021-04-13 23:36:55 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -436,6 +436,17 @@
 #     print(greetings[i])
 #     i += 1
 
+
 # 리스트 실습 2
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
 
+temperature_list = [40, 15, 32, 64, -4, 11]  # 화씨 온도
+print("화씨 온도 리스트: " + str(temperature_list))  # 화씨 온도 출력
 
+i = 0
+while i < len(temperature_list):
+    temperature_list[i] = round(fahrenheit_to_celsius(temperature_list[i]) ,1)
+    i += 1
+
+print("섭씨 온도 리스트: " + str(temperature_list))  # 섭씨 온도 출력
