d21659161617a80568e4794bb0197aa9da306e81
suxi519 <gksthgml75@naver.com>
2021-04-14 12:42:53 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -468,7 +468,7 @@ def usd_to_jpy(usd):
 prices = [34000, 13000, 5000, 21000, 1000, 2000, 8000, 3000]
 print("한국 화폐: " + str(prices))
 
-i = 1
+i = 0
 while i < len(prices):
     prices[i] = krw_to_usd(prices[i])
     i += 1
@@ -476,8 +476,10 @@ while i < len(prices):
 # 달러($)로 각각 얼마인가요?
 print("미국 화폐: " + str(prices))
 
-# amounts를 달러($)에서 엔화(￥)으로 변환하기
-# 코드를 입력하세요.
+i = 0
+while i < len(prices):
+    prices[i] = usd_to_jpy(prices[i])
+    i += 1
 
 # 엔화(￥)으로 각각 얼마인가요?
-# print("일본 화폐: " + str(prices))
+print("일본 화폐: " + str(prices))
