de623632ba47b8e80f0d1dd29c9ecc7a33de4edb
suxi519 <gksthgml75@naver.com>
2021-04-14 13:06:40 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -455,31 +455,61 @@
 
 # 리스트 연습 3
 # 원화(￦)에서 달러($)로 변환하는 함수
-def krw_to_usd(krw):
-    return krw / 1000
+# def krw_to_usd(krw):
+#     return krw / 1000
 
 
 # 달러($)에서 엔화(￥)로 변환하는 함수
-def usd_to_jpy(usd):
-    return usd * 125
+# def usd_to_jpy(usd):
+#     return usd * 125
 
 
 # 원화(￦)으로 각각 얼마인가요?
-prices = [34000, 13000, 5000, 21000, 1000, 2000, 8000, 3000]
-print("한국 화폐: " + str(prices))
+# prices = [34000, 13000, 5000, 21000, 1000, 2000, 8000, 3000]
+# print("한국 화폐: " + str(prices))
+#
+# i = 0
+# while i < len(prices):
+#     prices[i] = krw_to_usd(prices[i])
+#     i += 1
+#
+# # 달러($)로 각각 얼마인가요?
+# print("미국 화폐: " + str(prices))
+#
+# i = 0
+# while i < len(prices):
+#     prices[i] = usd_to_jpy(prices[i])
+#     i += 1
+#
+# # 엔화(￥)으로 각각 얼마인가요?
 
-i = 0
-while i < len(prices):
-    prices[i] = krw_to_usd(prices[i])
-    i += 1
 
-# 달러($)로 각각 얼마인가요?
-print("미국 화폐: " + str(prices))
+# print("일본 화폐: " + str(prices))
 
-i = 0
-while i < len(prices):
-    prices[i] = usd_to_jpy(prices[i])
-    i += 1
+# 리스트 연습 4
+# 빈 리스트 만들기
+numbers = []
+# print(numbers)
 
-# 엔화(￥)으로 각각 얼마인가요?
-print("일본 화폐: " + str(prices))
+numbers.append(1)
+numbers.append(7)
+numbers.append(3)
+numbers.append(6)
+numbers.append(5)
+numbers.append(2)
+numbers.append(13)
+numbers.append(14)
+numbers.sort()
+print(numbers)
+
+# numbers에서 홀수 제거
+# 코드를 입력하세요
+# print(numbers)
+
+# numbers의 인덱스 0 자리에 20이라는 값 삽입
+# 코드를 입력하세요
+# print(numbers)
+
+# numbers를 정렬해서 출력
+# 코드를 입력하세요
+# print(numbers)
