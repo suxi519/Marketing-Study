96612a881f168fa8572e3fd981785283ea31316b
suxi519 <gksthgml75@naver.com>
2021-04-14 12:38:25 +0900

    Update code.py

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -438,15 +438,46 @@
 
 
 # 리스트 실습 2
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5 / 9
+# def fahrenheit_to_celsius(fahrenheit):
+#     return (fahrenheit - 32) * 5 / 9
+#
+#
+# temperature_list = [40, 15, 32, 64, -4, 11]  # 화씨 온도
+# print("화씨 온도 리스트: " + str(temperature_list))  # 화씨 온도 출력
+#
+# i = 0
+# while i < len(temperature_list):
+#     temperature_list[i] = round(fahrenheit_to_celsius(temperature_list[i]) ,1)
+#     i += 1
+#
+#
+# print("섭씨 온도 리스트: " + str(temperature_list))  # 섭씨 온도 출력
 
-temperature_list = [40, 15, 32, 64, -4, 11]  # 화씨 온도
-print("화씨 온도 리스트: " + str(temperature_list))  # 화씨 온도 출력
+# 리스트 연습 3
+# 원화(￦)에서 달러($)로 변환하는 함수
+def krw_to_usd(krw):
+    return krw / 1000
 
-i = 0
-while i < len(temperature_list):
-    temperature_list[i] = round(fahrenheit_to_celsius(temperature_list[i]) ,1)
+
+# 달러($)에서 엔화(￥)로 변환하는 함수
+def usd_to_jpy(usd):
+    return usd * 125
+
+
+# 원화(￦)으로 각각 얼마인가요?
+prices = [34000, 13000, 5000, 21000, 1000, 2000, 8000, 3000]
+print("한국 화폐: " + str(prices))
+
+i = 1
+while i < len(prices):
+    prices[i] = krw_to_usd(prices[i])
     i += 1
 
-print("섭씨 온도 리스트: " + str(temperature_list))  # 섭씨 온도 출력
+# 달러($)로 각각 얼마인가요?
+print("미국 화폐: " + str(prices))
+
+# amounts를 달러($)에서 엔화(￥)으로 변환하기
+# 코드를 입력하세요.
+
+# 엔화(￥)으로 각각 얼마인가요?
+# print("일본 화폐: " + str(prices))
