a3562a63a4a1447d393ce4eb1130c1f7a8f77e29
suxi519 <gksthgml75@naver.com>
2021-04-22 16:39:11 +0900

    사전

--- 02_Base_Programming_in_Python/code.py
+++ 02_Base_Programming_in_Python/code.py
@@ -609,26 +609,45 @@
 # }
 # print(my_family['엄마'])
 
-my_family = {
-     '엄마' : '김공주',
-     '아빠' : '이용왕',
-     '아들' : '이왕자',
-     '딸' : '이거지'
-}
+# my_family = {
+#      '엄마' : '김공주',
+#      '아빠' : '이용왕',
+#      '아들' : '이왕자',
+#      '딸' : '이거지'
+# }
+#
+# print('이거지' in my_family.values())
+#
+# for value in my_family.values():
+#     print(value)
+#
+# print('아들' in my_family.keys())
+#
+# for value in my_family.keys():
+#     print(value)
+#
+# for key in my_family.keys():
+#     value = my_family[key]
+#     print(key, value)
+#
+# for key, value in my_family.items():
+#     print(key, value)
+#
 
-print('이거지' in my_family.values())
+x = 5
+y = x
+y = 3
+print(x)
+print(y)
 
-for value in my_family.values():
-    print(value)
+x = [2, 3, 5, 7, 11]
+y = x
+y[2] = 4
+print(x)
+print(y)
 
-print('아들' in my_family.keys())
-
-for value in my_family.keys():
-    print(value)
-
-for key in my_family.keys():
-    value = my_family[key]
-    print(key, value)
-
-for key, value in my_family.items():
-    print(key, value)
+x = [2, 3, 5, 7, 11]
+y = list(x)
+y[2] = 4
+print(x)
+print(y)
